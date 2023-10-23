ffe598673b894c919754281c2ae55f95a0182e6b
Mohammad Golchin <Golchin.muhammad@gmail.com>
2023-10-23 19:17:53 +0330

    Add project files.

---
+++ CreateHugeFile/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        long fileSizeInBytes = 1000000000; // به عنوان مثال 1 گیگابایت
+
+        byte[] largeByteArray = new byte[fileSizeInBytes];
+
+        Random random = new Random();
+        random.NextBytes(largeByteArray);
+
+        string filePath = "D:\\hugeByteArray10G.dat"; 
+
+        File.WriteAllBytes(filePath, largeByteArray);
+
+        Console.WriteLine($"فایل به مسیر '{filePath}' ذخیره شد.");
+        Console.ReadLine();
+    }
+
+}
---
+++ download resumable file/Controllers/DownloadController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System.IO;
+
+[Route("api/download")]
+public class DownloadController : ControllerBase
+{
+    private readonly IFileProvider _fileProvider;
+
+    public DownloadController(IFileProvider fileProvider)
+    {
+        _fileProvider = fileProvider;
+    }
+
+    [HttpGet("{fileName}")]
+    public IActionResult Download(string fileName, long? start)
+    {
+        var filePath = Path.Combine("D:\\", fileName);
+
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
+
+        var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+
+        if (start.HasValue)
+        {
+            fileStream.Seek(start.Value, SeekOrigin.Begin);
+        }
+
+        var fileLength = fileStream.Length - start.GetValueOrDefault();
+        var response = File(fileStream, "application/octet-stream", enableRangeProcessing: true);
+        response.EnableRangeProcessing = true;
+
+        Response.Headers.Add("Accept-Ranges", "bytes");
+
+        if (start.HasValue)
+        {
+            Response.Headers.Add("Content-Range", new StringValues($"{start}-{fileLength - 1}/{fileStream.Length}"));
+            
+        }
+        Response.Headers.Add("Content-Length", fileLength.ToString());
+
+        return response;
+    }
+}
